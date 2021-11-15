b6cd5d82ada431f0c9c672cf02177e73450dca84
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-15 13:36:36 +0200

    errors

---
+++ Errors/handler.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from random import randrange
+
+
+def main():
+    number = randrange(100)
+    while True:
+        try:
+            guess = int(input("? "))
+        except:
+            continue
+        if guess == number:
+            print("You win!")
+            break
+
+
+if __name__ == '__main__':
+    main()
