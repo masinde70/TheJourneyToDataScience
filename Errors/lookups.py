1568c4083cffaae91872be5ab26f9071fb47b1e5
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-15 14:14:35 +0200

    KeyError

--- Errors/lookups.py
+++ Errors/lookups.py
@@ -4,3 +4,9 @@ def lookups():
         item = s[5]
     except IndexError:
         print("Handled Index Error")
+
+    d = dict(a=65, b=66, c=67)
+    try:
+        value = d['x']
+    except KeyError:
+        print("Handled KeyError")
