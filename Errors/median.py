df7b333f3f0e43865109b0bf8e176ec261ffd013
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-16 07:19:19 +0200

    we added the guard which tells exactly where the problem occurs

--- Errors/median.py
+++ Errors/median.py
@@ -11,6 +11,8 @@ def median(iterable):
     """
 
     items = sorted(iterable)
+    if len(items) == 0:
+        raise ValueError("median() arg is empty series")
     median_index = (len(items) - 1) // 2
     if len(items) % 2 != 0:
         return items[median_index]
