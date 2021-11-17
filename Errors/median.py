af9d6c09f6cead159c513099ed6a464bed83a3e2
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-17 11:17:52 +0200

    accessing payloads through args.

--- Errors/median.py
+++ Errors/median.py
@@ -17,3 +17,14 @@ def median(iterable):
     if len(items) % 2 != 0:
         return items[median_index]
     return items[median_index] + items[median_index + 1] / 2.0
+
+
+def main():
+    try:
+        median([])
+    except ValueError as e:
+        print("Payload", e.args)
+
+if __name__ == '__main__':
+    main()
+
