b048ae670ac09b255c785750700262ec05dc4878
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-17 11:44:47 +0200

    enriching payloads

--- Errors/heron.py
+++ Errors/heron.py
@@ -21,7 +21,7 @@ class TriangleError(Exception):
 def triangle_area(a, b, c):
     sides = sorted((a, b, c))
     if sides[2] > sides[0] + sides[1]:
-        raise TimeoutError("Illegal triangle")
+        raise TimeoutError("Illegal triangle", sides)
 
     p = (a + b + c) / 2
     a = math.sqrt(p * (p - a) * (p - b) * (p - c))
