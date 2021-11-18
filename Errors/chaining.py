470a69dc2c10e323028c5298558b65375a8ece7b
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-18 07:06:32 +0200

    adding new exception type which is InclinationError

--- Errors/chaining.py
+++ Errors/chaining.py
@@ -1,8 +1,14 @@
 import math
 
 
+class InclinationError(Exception):
+    pass
+
+
 def inclination(dx, dy):
     """ The function returns the slope in degrees given the horizontal and vertical distance components
 
     """
     return math.degrees(math.atan(dx / dy))
+
+
