b7397cb6ff5348daf0bb865a0207017854b73cc1
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-18 07:04:29 +0200

    the function returns slope in degrees but fails with zero division error

--- Errors/chaining.py
+++ Errors/chaining.py
@@ -2,4 +2,7 @@ import math
 
 
 def inclination(dx, dy):
+    """ The function returns the slope in degrees given the horizontal and vertical distance components
+
+    """
     return math.degrees(math.atan(dx / dy))
