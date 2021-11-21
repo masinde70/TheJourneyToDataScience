105b3031977e5fd7d8ba801afc77c2478d1948d9
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-21 12:38:21 +0200

    sized protocol

--- Errors/test_sorted_set.py
+++ Errors/test_sorted_set.py
@@ -42,5 +42,23 @@ class TestContainerProtocol(unittest.TestCase):
     def test_negative_not_contained(self):
         self.assertFalse(9 not in self.s)
 
+class TestSizedProtocol(unittest.TestCase):
+    def test_empty(self):
+        s = SortedSet()
+        self.assertEqual(len(s), 0)
+
+    def test_one(self):
+        s = SortedSet([42])
+        self.assertEqual(len(s), 1)
+
+    def test_ten(self):
+        s = SortedSet(range(10))
+        self.assertEqual(len(s), 10)
+
+    def test_with_duplicates(self):
+        s = SortedSet([5, 5, 5])
+        self.assertEqual(len(s), 1)
+
+
 if __name__ == '__main__':
     unittest.main()
