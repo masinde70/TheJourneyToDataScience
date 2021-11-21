41407cd36a8368284a6efb254d3bc6d9b0d3790f
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-21 17:31:39 +0200

    set relation protocol

--- Errors/test_sorted_set.py
+++ Errors/test_sorted_set.py
@@ -232,6 +232,11 @@ class TestRelationalSetProtocol(unittest.TestCase):
         t = SortedSet({1, 2, 3})
         self.assertTrue(s < t)
 
+    def test_lt_negative(self):
+        s = SortedSet({1, 2, 3})
+        t = SortedSet({1, 2, 3})
+        self.assertFalse(s < t)
+
 
 
 
