98afdd87a4c8a323df753453a6222baabe3a2f5c
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-21 12:40:30 +0200

    sized protocol

--- Errors/sorted_set.py
+++ Errors/sorted_set.py
@@ -1,7 +1,7 @@
 class SortedSet:
 
     def __init__(self, items=None):
-        self._items = sorted(items) if items is not None else []
+        self._items = sorted(set(items)) if items is not None else []
 
     def __contains__(self, item):
         return item in self._items
