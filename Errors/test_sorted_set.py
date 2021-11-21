aa995936b386a0b62fba7a6cb39b05cbf7e1d1d5
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-21 17:37:45 +0200

    set relation methods

--- Errors/test_sorted_set.py
+++ Errors/test_sorted_set.py
@@ -290,6 +290,21 @@ class TestSetRelationalMethods(unittest.TestCase):
         t = [1, 2, 3]
         self.assertTrue(s.issubset(t))
 
+    def test_issubset_negative(self):
+        s = SortedSet({1, 2, 3})
+        t = [1, 2]
+        self.assertFalse(s.issubset(t))
+
+    def test_issuperset_proper_positive(self):
+        s = SortedSet({1, 2, 3})
+        t = [1, 2]
+        self.assertTrue(s.issuperset(t))
+
+    def test_issuperset_positive(self):
+        s = SortedSet({1, 2, 3})
+        t = [1, 2, 3]
+        self.assertTrue(s.issuperset(t))
+
 
 
 
