8c550b05b0ecf6df0fcca7e380a09ef9ac2ce894
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-22 07:19:01 +0200

    added assert condition

---
+++ Errors/wrap.py
@@ -0,0 +1,24 @@
+def wrap(text, line_length):
+    """Wrap a string to a specified line length
+
+    Args:
+        text: The string to wrap
+        line_length: The line length in characters.
+
+    Returns:
+        A wrapped string.
+
+    """
+    words = text.split()
+    lines_of_words = []
+    current_line_length = line_length
+    for word in words:
+        if current_line_length + len(word) > line_length:
+            lines_of_words.append([])
+            current_line_length = 0
+            lines_of_words[-1].append(word)
+            current_line_length += len(word)
+    lines = [' '.join(lines_of_words) for lines_of_words in lines_of_words]
+    result = '\n'.join(lines)
+    assert all(len(line) <= line_length for line in result.splitlines())
+    return result
