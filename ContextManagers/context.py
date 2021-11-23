72dc1be54c3d827736e8e0bbb477ae370cbe4b87
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-23 14:44:10 +0200

    context __enter__ returning a statement

--- ContextManagers/context.py
+++ ContextManagers/context.py
@@ -1,6 +1,7 @@
 class LoggingContextManager:
+
     def __enter__(self):
-        return self
+        return "You're in a with-block!"
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         return
