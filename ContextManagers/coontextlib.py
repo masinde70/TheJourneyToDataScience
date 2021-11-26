0137a9c77efb2a92463dbdae95dd0fbd85c3a085
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-26 11:34:28 +0200

    the context manager decorator allows you to define context-managers control flow in a single function via yield statement

---
+++ ContextManagers/coontextlib.py
@@ -0,0 +1,12 @@
+import contextlib
+import sys
+
+
+@contextlib.contextmanager
+def logging_context_manager():
+    print('logging_context_manager: enter')
+    try:
+        yield "You're in a with-block"
+        print('logging_context_manager: normal exit')
+    except Exception:
+        print('logging_context_manager: exception exit', sys.exc_info())
