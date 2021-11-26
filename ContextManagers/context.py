f69c846384f5b9c06c80beef1fc526c3d6168c83
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-26 10:50:35 +0200

    Logging context manager __exit__ checking if an exception was raised

--- ContextManagers/context.py
+++ ContextManagers/context.py
@@ -5,6 +5,12 @@ class LoggingContextManager:
         return "You're in a with-block!"
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print('LoggingContextManager.__exit__({}, {}, {})'.format(exc_type, exc_val, exc_tb))
+        if exc_type is None:
+            print('LoggingContextManager.__exit__:'
+                  'normal exit detected')
+        else:
+            print('LoggingContextManager.__exit__:'
+                  'Exception detected! '
+                  'type={}, value={}, traceback={}'.format(exc_type, exc_val, exc_tb))
         return
 
