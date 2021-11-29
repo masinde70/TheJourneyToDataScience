2d2272277ccb0f02a1e4907a11b5316c134545e7
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-11-29 11:13:21 +0200

    context manager for transaction

--- ContextManagers/transaction.py
+++ ContextManagers/transaction.py
@@ -2,3 +2,12 @@ class Connection:
     """This connection class represents some sort of database connection.
 
     """
+    def __init__(self):
+        self.xid = 0
+
+    def _start_transaction(self):
+        print('start transaction', self.xid)
+        rslt = self.xid
+        self.xid = self.xid + 1
+        return rslt
+
