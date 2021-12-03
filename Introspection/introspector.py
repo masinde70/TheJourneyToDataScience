e9de2ff60da29d71f725a067d5b7cef2d0c124e8
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-12-03 09:56:30 +0200

    introspect brief doc

--- Introspection/introspector.py
+++ Introspection/introspector.py
@@ -29,3 +29,11 @@ def full_sig(method):
     except ValueError:
         return method.__name__ + '(...)'
 
+
+def brief_doc(obj):
+    doc = obj.__doc__
+    if doc is not None:
+        lines = doc.splitlines()
+        if len(lines) > 0:
+            return lines[0]
+    return ''
