4b44117c918540f394c73e6963fb4cac9968c2bc
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-12-03 09:45:10 +0200

    introspect

---
+++ Introspection/introspector.py
@@ -0,0 +1,21 @@
+def dump(obj):
+    print("Type")
+    print("===")
+    print(type(obj))
+    print()
+
+    print("Documentation")
+    print("========")
+    # TODO
+    print()
+
+    print("Attributes")
+    print("==========")
+    # TODO
+    print()
+
+    print("Methods")
+    print("=======")
+    # TODO
+    print()
+
