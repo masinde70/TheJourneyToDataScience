313c8a83f29d209f60c08cea54dd24b69889b1e3
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-12-06 07:06:59 +0200

    added sorted set

--- Introspection/introspector.py
+++ Introspection/introspector.py
@@ -1,63 +1,27 @@
-import inspect
 import itertools
 import reprlib
+import inspect
+from sorted_set import SortedSet
 
 
 def dump(obj):
     print("Type")
-    print("===")
+    print("====")
     print(type(obj))
     print()
 
     print("Documentation")
-    print("========")
-    print(inspect.getdoc(obj))
+    print("=============")
+    print(obj.__doc__)
     print()
+
+    all_attr_names = SortedSet(dir(obj))
+    method_names = SortedSet(
+        filter(lambda attr_name: callable(getattr(obj, attr_name)),
+               all_attr_names))
+    assert method_names <= all_attr_names
+    attr_names = all_attr_names - method_names
 
     print("Attributes")
     print("==========")
-    all_attr_names = SortedSet(dir(obj))
-    method_names = SortedSet(filter(lambda attr_name:
-                                    callable(getattr(obj, attr_name)),
-                                    all_attr_names))
-    print()
-
-    print("Methods")
-    print("=======")
-    assert method_names <= all_attr_names
-    attr_names = all_attr_names - method_names
-    attr_names_and_values = [(name, reprlib.repr(getattr(object, name))) for name in all_attr_names]
-    print_table(attr_names_and_values, "Name", "Value")
-    methods = (getattr(obj, method_name) for method_name in method_names)
-    methods_names_and_doc = [(full_sig(method), brief_doc(method)) for method in methods]
-    print_table(methods_names_and_doc, "Name", "Description")
-    print()
-    
-   
-def full_sig(method):
-    try:
-        return method.__name__ + inspect.signature(method)
-    except ValueError:
-        return method.__name__ + '(...)'
-
-
-def brief_doc(obj):
-    doc = obj.__doc__
-    if doc is not None:
-        lines = doc.splitlines()
-        if len(lines) > 0:
-            return lines[0]
-    return ''
-
-
-def print_table(rows_of_columns, *headers):
-    num_columns = len(rows_of_columns[0])
-    num_headers = len(headers)
-    if len(headers) != num_columns:
-        raise TypeError("Expected {} header arguments, "
-                        "got {}".format(num_columns, num_headers))
-
-    rows_of_columns_with_header = itertools.chain([headers], rows_of_columns)
-    columns_of_rows = list(zip(*rows_of_columns_with_header))
-    column_widths = [max(map(len, column)) for column in columns_of_rows]
-
+    attr_names_and_values = [(name, reprlib.repr(getattr(obj, name)))
---
+++ Introspection/sorted_set.py
@@ -0,0 +1,75 @@
+from collections.abc import Sequence, Set
+from bisect import bisect_left
+from itertools import chain
+
+
+class SortedSet(Sequence, Set):
+
+    def __init__(self, items=None):
+        self._items = sorted(set(items)) if items is not None else []
+
+    def __contains__(self, item):
+        try:
+            self.index(item)
+            return True
+        except ValueError:
+            return False
+
+    def __len__(self):
+        return len(self._items)
+
+    def __iter__(self):
+        yield from self._items
+
+    def __getitem__(self, index):
+        result = self._items[index]
+        return SortedSet(result) if isinstance(index, slice) else result
+
+    def __repr__(self):
+        return "SortedSet({})".format(repr(self._items) if self._items else '')
+
+    def __eq__(self, rhs):
+        return self._items == rhs._items
+
+    def __eq__(self, rhs):
+        if not isinstance(rhs, SortedSet):
+            return NotImplemented
+        return self._items == rhs._items
+
+    def _is_unique_and_sorted(self):
+        return all(self[i] < self[i + 1] for i in range(len(self) - 1))
+
+    def count(self, item):
+        assert self._is_unique_and_sorted()
+        return int(item in self._items)
+
+    def index(self, item):
+        assert self._is_unique_and_sorted()
+        index = bisect_left(self._items, item)
+        if (index != len(self._items)) and self._items[index] == item:
+            return index
+        raise ValueError("{} not found".format(repr(item)))
+
+    def __add__(self, rhs):
+        return SortedSet(chain(self._items, rhs._items))
+
+    def __rmul__(self, lhs):
+        return self * lhs
+
+    def issubset(self, iterable):
+        return self <= SortedSet(iterable)
+
+    def issuperset(self, iterable):
+        return self >= SortedSet(iterable)
+
+    def intersection(self, iterable):
+        return self & SortedSet(iterable)
+
+    def union(self, iterable):
+        return self | SortedSet(iterable)
+
+    def symmetric_difference(self, iterable):
+        return self ^ SortedSet(iterable)
+
+    def difference(self, iterable):
+        return self - SortedSet(iterable)
