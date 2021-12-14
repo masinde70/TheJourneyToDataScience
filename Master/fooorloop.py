18f243ca6f21deb2076e41df7f7606c7dd783e25
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-12-14 09:00:44 +0200

    for else loop

---
+++ Master/fooorloop.py
@@ -0,0 +1,14 @@
+def divisor():
+    items = [2, 25, 9, 37, 28, 14]
+    diivisor = 12
+
+    for item in items:
+        if item % diivisor == 0:
+            found = item
+            break
+    else:
+        items.append(diivisor)
+        found = diivisor
+
+    print("{items} contains {found} which is a multiple"
+          f"of {diivisor}".format(**locals()))
