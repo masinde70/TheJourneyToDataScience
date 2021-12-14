4d4e1688bbe8f3680ab48f00ce6d8f4494235f66
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-12-14 09:13:08 +0200

    for else loop alternative which can be easly read by normal python programmer not some advanced features of for..else

--- Master/fooorloop.py
+++ Master/fooorloop.py
@@ -12,3 +12,19 @@ def divisor():
 
     print("{items} contains {found} which is a multiple"
           f"of {diivisor}".format(**locals()))
+
+
+def ensure_has_divisible(items, divisor):
+    for item in items:
+        if item % divisor == 0:
+            return item
+    items.append(divisor)
+    return divisor
+
+
+items = [2, 25, 9, 37, 24, 14]
+divisor = 12
+
+dividend = ensure_has_divisible(items, divisor)
+print(f"{items} contains {dividend} which is a multiple of"
+      "{divisor}".format(**locals()))
