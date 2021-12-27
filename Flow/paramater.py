0204bf449f8642b7f1ab1c27ba0305003c36744f
masinde70 <5812481+masinde70@users.noreply.github.com>
2021-12-27 10:44:02 +0200

    metaflow parameters

--- Flow/paramater.py
+++ Flow/paramater.py
@@ -2,13 +2,29 @@ from metaflow import FlowSpec, Parameter, step
 
 
 class ParameterFlow(FlowSpec):
-     animal = Parameter('creature',
-                        help="Specify a creature",
-                        required=True)
-     count = Parameter('count',
-                       help="Number of creatures",
-                       default=1)
+    animal = Parameter('creature',
+                       help="Specify a creature",
+                       required=True)
+    count = Parameter('count',
+                      help="Number of creatures",
+                      default=1)
 
-     ratio = Parameter('ratio',
-                       help="Ratio between 0.0 and 1.0",
-                       type=float)
+    ratio = Parameter('ratio',
+                      help="Ratio between 0.0 and 1.0",
+                      type=float)
+
+    @step
+    def start(self):
+        print(self.animal, "is a string of", len(self.animal), "characters")
+        print("Count is an integer: %s+1=%s" % (self.count, self.count + 1))
+        print("Ratio is a", type(self.ratio), "whose value is", self.ratio)
+        self.next(self.end)
+
+    @step
+    def end(self):
+        print('done!')
+
+
+if __name__ == '__main__':
+    ParameterFlow()
+
