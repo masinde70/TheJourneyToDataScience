90a6080666a9688c7d3a2d3d9753758065bf104b
masinde70 <5812481+masinde70@users.noreply.github.com>
2022-01-13 10:02:55 +0200

    dispatch type

--- Master/shapes.py
+++ Master/shapes.py
@@ -3,8 +3,13 @@ class Shape:
     def __init__(self, solid):
         self.solid = solid
 
+
 class Circle(Shape):
 
     def __init__(self, center, radius, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.center = center
+        self.radius = radius
+
 
 
